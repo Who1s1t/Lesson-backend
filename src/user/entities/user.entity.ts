5de3e9cb9e7daf3328473d38da6804f115d0d5b6
OVER <dedsek965@gmail.com>
2023-11-05 00:18:33 +0300

    add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity("user")
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    role: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    surName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    birthday: string;
+
+
+
+
+
+
+}
