65642c138c21fdcc040b5b07821df349155bfa8a
OVER <dedsek965@gmail.com>
2023-11-05 02:16:00 +0300

    update user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import {IsEmail, IsInt, IsNotEmpty, IsString, MinLength} from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
+
+export class CreateUserDto {
+    @IsString()
+    @ApiProperty()
+    firstName:string
+
+    @IsString()
+    @ApiProperty()
+    surName: string
+
+
+    @IsString()
+    @ApiProperty()
+    lastName: string
+
+    @IsString()
+    @ApiProperty()
+    birthday: string
+
+    @IsEmail()
+    @ApiProperty()
+    email: string
+
+    @MinLength(8, {message: "Пароль не может быть короче 8 символов!"})
+    @ApiProperty()
+    password: string
+
+}
